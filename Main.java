4f9c37542b872a14c98f637b4138a2767bafe535
Lutfi Elyas <90002177+lutfielyas@users.noreply.github.com>
2022-05-24 13:02:37 +0700

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-package Responsi;
-
-public class Main {
-    public static void main(String[] args) {
-        MovieView mv = new MovieView();
-        ModelMovie mm = new ModelMovie();
-        ControllerMovie cm = new ControllerMovie(mm, mv);
-    }
-}
